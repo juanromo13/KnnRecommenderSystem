72643a86d0df8ca17d63234179912e209c548cd8
juanromo13 <juanochiro1315@gmail.com>
2022-05-21 12:35:33 -0500

    correccion de eror

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ X = | 1 2 0 1 3 | user 1 no califico la pelicula 2
     | 5 0 0 2 4 | user 2 no califico la pelicula 1 y 2
     | 1 2 5 0 1 | user 3 no califico la pelicula 3
 
-K = numero tipos de usuarios
+K = numero de usuarios similares 
 """
 
 X = np.loadtxt("../test_incomplete.txt")
